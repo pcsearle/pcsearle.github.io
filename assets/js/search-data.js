3fb2772bce50bc91bc98baf65190b56f5afdaa9d
pcsearle <83605294+pcsearle@users.noreply.github.com>
2025-10-11 02:39:12 +0000

    Deploying to gh-pages from @ pcsearle/pcsearle.github.io@8ac2b5a2029825f672f3e481ba32006d944d3972 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,11 +24,11 @@ ninja.data = [{
             window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/CV/";
           },
         },{id: "nav-people",
           title: "people",
