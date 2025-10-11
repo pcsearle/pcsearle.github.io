0ddbbebbada4bf6a576d8c92c2fafda6c2f0cee7
pcsearle <83605294+pcsearle@users.noreply.github.com>
2025-10-11 03:22:42 +0000

    Deploying to gh-pages from @ pcsearle/pcsearle.github.io@157a2e3bc79d50e00796c4bf9c03a3c84419875b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "true",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
