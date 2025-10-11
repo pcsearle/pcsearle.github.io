1c222e0af9c0749ddb5604693199ecde52b33bb0
pcsearle <83605294+pcsearle@users.noreply.github.com>
2025-10-11 03:05:55 +0000

    Deploying to gh-pages from @ pcsearle/pcsearle.github.io@9fb98520385da7dfd73c026f80ff742c2fe4e0df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,14 +17,14 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
