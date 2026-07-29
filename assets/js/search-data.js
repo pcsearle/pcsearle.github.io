697ebb22ba32cad9addb0acd8ef9364b4c1b5398
pcsearle <83605294+pcsearle@users.noreply.github.com>
2026-07-29 02:29:54 +0000

    Deploying to gh-pages from @ pcsearle/pcsearle.github.io@e634d079e9d9974c40c05339ebbc5909de9bf8e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -70,7 +70,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=2PcBi4YAAAAJ&hl", "_blank");
+          window.open("https://scholar.google.com/citations?user=2PcBi4YAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
