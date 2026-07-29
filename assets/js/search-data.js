ca7da47a84d7d7dba45d026d99ace3b5f4397d30
pcsearle <83605294+pcsearle@users.noreply.github.com>
2026-07-29 02:41:36 +0000

    Deploying to gh-pages from @ pcsearle/pcsearle.github.io@8cd85ef3c1312daed0d2d8002bc0be6c11bbd9e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,23 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-visual-evolution-in-rockfish",
+        },{id: "projects-visual-evolution-in-rockfish",
           title: 'Visual Evolution in Rockfish',
           description: "",
           section: "Projects",handler: () => {
